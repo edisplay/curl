caf7e88941ba0882fb2ffc237a600c148a66e624
Jeroen Ooms <jeroenooms@gmail.com>
2026-04-18 22:30:40 +0200

    Notify proxy server onattach

--- R/onload.R
+++ R/onload.R
@@ -7,4 +7,12 @@
     packageStartupMessage(c("Your system runs libcurl-minimal which does not support all protocols: ",
                           "See also https://github.com/jeroen/curl/issues/350"))
   }
+  try({
+    proxy <- Sys.getenv('ALL_PROXY')
+    if(nchar(proxy)){
+      proxy_info <- curl::curl_parse_url(proxy)
+      packageStartupMessage(sprintf("Using proxy server %s://%s:%s",
+        proxy_info$scheme, proxy_info$host, proxy_info$port))
+    }
+  }, silent = TRUE)
 }
